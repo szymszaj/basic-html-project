de1ef5cc6679566d2d0512cde1b1643e53c9aa27
Szymon Zych <szymonzych868@gmail.com>
2023-05-28 23:11:03 +0200

    update js

--- Physics Game/script.js
+++ Physics Game/script.js
@@ -1,7 +1,23 @@
 window.addEventListener('load', function (){
     const canvas = document.getElementById('canvas1')
     const ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
     canvas.width = 1280; 
     canvas.height = 720;
+
+
+    const game = new Game(canvas);
+    game.init();
+    console.log(game);
+    
+    function animate(){
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate);
+        
+        animate();
+    }
      
 });
