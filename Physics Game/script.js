73d5df706782b876bdd34243b293758d0895505b
Szymon Zych <szymonzych868@gmail.com>
2023-06-09 15:32:45 +0200

    mouse controls

--- Physics Game/script.js
+++ Physics Game/script.js
@@ -31,8 +31,33 @@ window.addEventListener('load', function (){
         constructor(canvas){
             this.canvas = canvas;
             this.width = this.canvas.width; 
-            this.height = this.canvas.height
-            this.player = new Player(this)
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
+            }
+
+
+            // event listeners
+            canvas.addEventListener('mousedown', e =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            });
+            canvas.addEventListener('mouseup', e =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+            canvas.addEventListener('mousemove', e =>{
+                this.mouse.x = e.offsetX;
+                this.mouse.y = e.offsetY;
+            });
+
+
+
         }
         render(context){
             this.player.draw(context);
