41d9b4233314f60702f3394db6ecb456ca28b98b
Szymon Zych <szymonzych868@gmail.com>
2023-06-09 16:34:21 +0200

    making the player move

--- Physics Game/script.js
+++ Physics Game/script.js
@@ -13,7 +13,13 @@ window.addEventListener('load', function (){
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 30;
+            this.collisionRadius = 50;
+            this.speedX = 0; 
+            this.speedY = 0; 
+            this.dx = 0;
+            this.dy = 0;
+            this.speedModifier = 20;
+
         }
         draw(context){
             context.beginPath();
@@ -24,7 +30,24 @@ window.addEventListener('load', function (){
                 context.fill();
                 context.restore();
                 context.stroke();
-             
+                context.beginPath();
+                context.moveTo(this.collisionX, this.collisionY);
+                context.lineTo(this.game.mouse.x, this.game.mouse.y);
+                context.stroke();
+        }
+        update(){
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY;
+            const distance = Math.hypot(this.dy, this.dx);
+            if (distance > this.speedModifier){
+                this.speedX = this.dx/distance || 0;
+                this.speedY = this.dy/distance || 0;
+            } else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
         }
     }
     class Game {
@@ -52,8 +75,11 @@ window.addEventListener('load', function (){
                 this.mouse.pressed = false;
             });
             canvas.addEventListener('mousemove', e =>{
-                this.mouse.x = e.offsetX;
-                this.mouse.y = e.offsetY;
+                if (this.mouse.pressed){
+                    this.mouse.x = e.offsetX;
+                    this.mouse.y = e.offsetY;
+                }
+               
             });
 
 
@@ -61,20 +87,21 @@ window.addEventListener('load', function (){
         }
         render(context){
             this.player.draw(context);
+            this.player.update()
         }
     }
 
     const game = new Game(canvas);
-    game.render(ctx);
-    console.log(game);
+
+
 
     function animate(){
-        // ctx.clearRect(0,0, canvas.width, canvas.height);
-        // game.render(ctx);
-        // window.requestAnimationFrame(animate);
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate);
         
-        // animate();
     }
+    animate();
     
 
 
