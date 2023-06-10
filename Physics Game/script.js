831f3739ee85624516e5c27f417f6dea32a2947c
Szymon Zych <szymonzych868@gmail.com>
2023-06-10 16:59:31 +0200

    adding items/Positioning rules

--- Physics Game/script.js
+++ Physics Game/script.js
@@ -56,9 +56,21 @@ window.addEventListener('load', function (){
             this.collisionX = Math.random() * this.game.width;
             this.collisionY = Math.random() * this.game.height;
             //rozmiar 
-            this. collisionRadius = 80;
+            this. collisionRadius = 60;
+            this.image = document.getElementById('obstacles');
+            this.spriteWidth = 250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5
+            -70;
+
         }
         draw(context){
+            context.drawImage(this.image, 0, 0, this.spriteWidth, 
+                this.spriteHeight, this.spriteX, this.spriteY,
+                this.width, this.height);
             context.beginPath();
             context.arc(this.collisionX, this.collisionY, 
                 this.collisionRadius, 0, Math.PI * 2);
