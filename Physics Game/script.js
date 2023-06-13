c2fe4ca0ad4ac966a909aa795cc7a5b013560825
Szymon Zych <szymonzych868@gmail.com>
2023-06-13 12:50:19 +0200

    collisions with obstacles

--- Physics Game/script.js
+++ Physics Game/script.js
@@ -48,6 +48,12 @@ window.addEventListener('load', function (){
             }
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
+            //zderzenia z przeszkodami
+            this.game.obstacles.forEach(obstacle => {
+                 if(this.game.checkCollision(this, obstacle)){
+                    console.log('collision');
+                 };
+            })
         }
     }
     class Obstacle {
@@ -131,6 +137,14 @@ window.addEventListener('load', function (){
             this.player.update()
             this.obstacles.forEach(obstacle => obstacle.draw
                 (context));
+        }
+        checkCollision(a,b){
+            const dx = a.collisionX - b.collisionX;
+            const dy = a.collisionY - b.collisionY;
+            const distance = Math.hypot(dy, dx);
+            const sumOfRadii = a.collisionRadius +b.
+            collisionRadius;
+            return (distance < sumOfRadii);
         }
         init(){
             
