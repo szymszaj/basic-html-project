78c0584c33e5cd6a8253fbabbba7413c65502136
Szymon Zych <szymonzych868@gmail.com>
2023-06-24 14:15:34 +0200

    addition else/if

---
+++ operators in JS/add.js
@@ -0,0 +1,25 @@
+const pass = 'dsx!dbg34'
+
+if(pass.length > 10 && pass.includes('!')) {
+    console.log('Masz dobre hasło');
+} else  if (pass.length > 10) {
+    console.log('Masz za ok hasło');
+} else {
+    console.log('Masz za krótkie hasło');
+}
+
+
+
+
+
+
+// const color = 'blue'
+
+// if (color == 'blue' ){
+//     if ( 10 > 0 ){
+//         console.log('true true ');
+//     } else {
+//         console.log('okokok');
+//     }
+    
+// }
