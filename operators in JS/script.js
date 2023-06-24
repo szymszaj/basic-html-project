00b1c119749164a85cc40413acbf5334e515715a
Szymon Zych <szymonzych868@gmail.com>
2023-06-24 13:35:06 +0200

    using the operator

---
+++ operators in JS/script.js
@@ -0,0 +1,30 @@
+let  x = 5 
+const y = 8 
+const add = x + y 
+console.log(add);
+
+const substract = x - y 
+console.log(substract);
+
+const multiply = x * y 
+console.log(multiply);
+
+const divide = x / y 
+console.log(divide);
+
+// operator ++
+x++
+console.log(`Inkrementacja: ${x}`);
+x++
+console.log(`Inkrementacja: ${x}`);
+x++
+console.log(`Inkrementacja: ${x}`);
+
+//operator --
+x--
+x--
+console.log(x);
+
+//modulo %
+const modulo = 10 % 4
+console.log(modulo);
