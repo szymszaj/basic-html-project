69bce59a478e55fc5b25b7f3b2453b7d247b4e0d
Szymon Zych <szymonzych868@gmail.com>
2023-07-17 13:35:25 +0200

    adding a section with navigation

--- Responsive website/js/app.js
+++ Responsive website/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const nav = document.querySelector('.navbar')
     
     function addShadow() {
-        if (window.scrollY >= 100) {
+        if (window.scrollY >= 200) {
             nav.classList.add('shadow-bg')
         } else {
             nav.classList.remove('shadow-bg')
