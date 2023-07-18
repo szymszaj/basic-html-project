c41cd1a0e519d507c00f5a3bd9ced80bdf8db7c2
Szymon Zych <szymonzych868@gmail.com>
2023-07-19 00:02:59 +0200

    after pressing the button, the item will be added

--- TO-DO LIST/to-do list App/app.js
+++ TO-DO LIST/to-do list App/app.js
@@ -36,3 +36,38 @@ function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+
+
+
+// // Funkcja obsługująca zdarzenie naciśnięcia klawisza
+// function handleKeyPress(event) {
+//     // Sprawdź czy naciśnięty klawisz to Enter (kod 13)
+//     if (event.keyCode === 13) {
+//       event.preventDefault(); 
+//       // Zapobiegaj domyślnej akcji (np. wysyłania formularza)
+//       // Wywołaj funkcję dodawania rzeczy
+//       addThing();
+//     }
+//   }
+//   // Funkcja dodawania rzeczy
+//   function addThing() {
+//     // Pobierz wartość z pola tekstowego
+//     var input = document.getElementById("input-box");
+//     var thing = input.value;
+    
+//     // Sprawdź czy wartość jest niepusta
+//     if (thing.trim() !== "") {
+//       // Stwórz nowy element <li> dla rzeczy
+//       var item = document.createElement("list-container");
+//       item.textContent = thing;
+      
+//       // Dodaj nowy element do listy
+//       var list = document.getElementById("list-container");
+//       list.appendChild(item);
+      
+//     }
+//   }
+  
+//   // Przypisanie funkcji obsługi zdarzenia do zdarzenia "keypress" dla pola tekstowego
+//   document.getElementById("input-box").addEventListener("keypress", handleKeyPress);
