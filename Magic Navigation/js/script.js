b52856d41cc0d754084f68a33bb5628b441437ed
Szymon Zych <szymonzych868@gmail.com>
2023-07-20 23:53:32 +0200

    adding animation after pressing the button(firework animation)

--- Magic Navigation/js/script.js
+++ Magic Navigation/js/script.js
@@ -50,3 +50,33 @@ function addActiveClass() {
 
 item.forEach((i) => i.addEventListener('click', addActiveClass));
 
+
+
+// Creating fireworks
+function createFirework() {
+    // Create a <div> element for the firework
+    var firework = document.createElement("div");
+    firework.className = "firework";
+    
+    // Generate a random position for the firework on the screen
+    var left = Math.random() * window.innerWidth + "px";
+    var top = Math.random() * window.innerHeight + "px";
+    firework.style.left = left;
+    firework.style.top = top;
+    
+    // Add a firework to the body of the document
+    document.body.appendChild(firework);
+    
+    // Remove the firework after the animation ends
+    firework.addEventListener("animationend", function() {
+      firework.parentNode.removeChild(firework);
+    });
+  }
+  
+  // Handling the button click event
+  document.getElementById("fireworkButton").addEventListener("click", function() {
+    // Create 10 fireworks
+    for (var i = 0; i < 5; i++) {
+      createFirework();
+    }
+  });
