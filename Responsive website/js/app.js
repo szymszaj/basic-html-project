52157abc8eaada39b5a7bcf85cfce1befe9027cc
Szymon Zych <szymonzych868@gmail.com>
2023-07-21 13:24:16 +0200

    creating hover navigation

--- Responsive website/js/app.js
+++ Responsive website/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const nav = document.querySelector('.navbar')
     
     function addShadow() {
-        if (window.scrollY >= 200) {
+        if (window.scrollY >= 250) {
             nav.classList.add('shadow-bg')
         } else {
             nav.classList.remove('shadow-bg')
