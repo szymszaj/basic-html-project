84b76e8b1590124db1c0970fcee9305ce28383b0
Szymon Zych <szymonzych868@gmail.com>
2023-07-28 16:24:31 +0200

    creating buttons (edit, complete)

--- TO-DO LIST/responsive to do list/app.js
+++ TO-DO LIST/responsive to do list/app.js
@@ -2,7 +2,14 @@ let todoInput
 let errorInfo
 let addBtn
 let ulList
-let newTodo
+// let newTodo
+
+let popup
+let popupInfo
+let todoToEdit
+let popupInput
+let popupAddBtn
+let popupCloseBtn
 
 
 const main = () => {
@@ -15,23 +22,74 @@ const prepareDOMElements = () => {
     errorInfo = document.querySelector('.error-info')
     addBtn = document.querySelector('.btn-add')
     ulList = document.querySelector('.todolist ul')
+
+    popup = document.querySelector('.popup')
+    popupInfo = document.querySelector('.popup-info')
+    popupInput = document.querySelector('.popup-input')
+    popupAddBtn = document.querySelector('.accept')
+    popupCloseBtn = document.querySelector('.cancel')
+
 }
 
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTodo)
+    ulList.addEventListener('click', chcekClick)
+    popupCloseBtn.addEventListener('click', closePopup)
 }
 
 
 const addNewTodo = () => {
     if (todoInput.value !== ''){
-        newTodo = document.createElement('li')
+      const newTodo = document.createElement('li')
         newTodo.textContent = todoInput.value
-        ulList.append(newTodo)
+        createToolsArea(newTodo)
 
+        ulList.append(newTodo)
+       
         todoInput.value = ''
         errorInfo.textContent = ''
     } else {
         errorInfo.textContent = 'Wpisz treść zadania!'
     }
 }
+const createToolsArea = (newTodo) => {
+     const toolsPanel = document.createElement('div')   
+     toolsPanel.classList.add('tools')
+     newTodo.append(toolsPanel)
+
+     const completeBtn = document.createElement('button')
+     completeBtn.classList.add('complete')
+     completeBtn.innerHTML = '<i class="fas fa-check"></i></button>'
+
+     const editBtn = document.createElement('button')
+     editBtn.classList.add('edit')
+     editBtn.textContent = 'EDIT'
+
+     const deletBtn = document.createElement('button')
+     deletBtn.classList.add('delete')
+     deletBtn.innerHTML = '<i class="fas fas-times"></i></button>'
+
+     toolsPanel.append(completeBtn, editBtn, deletBtn)
+} 
+
+//spawdzanie który przycisk zostal wcisniety 
+const chcekClick = e => {
+    if(e.target.matches('.complete')) {
+     e.target.closest('li').classList.toggle('completed');
+     e.target.classList.toggle('completed')
+    } else if(e.target.matches('.edit')) {
+        editTodo()
+    } else if (e.target.matches('.delete')){
+        console.log('delete');
+    }
+}
+
+const editTodo = () => {
+    popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+    popup.style.display = 'none'
+}
+
 document.addEventListener('DOMContentLoaded', main)
