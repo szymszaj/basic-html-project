1c04b8316a118bb51744d38f0a9c19100005143c
Szymon Zych <szymonzych868@gmail.com>
2023-09-10 18:13:00 +0200

    adding a section with opening panels

--- Responsive multi-page website/main.js
+++ Responsive multi-page website/main.js
@@ -1,4 +1,19 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll',
-     window.scrollY > 110)
+        window.scrollY > 110)
+})
+
+//open faq
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq_icon i')
+        if(icon.className === 'fa-solid fa-plus') {
+            icon.className = 'fa-solid fa-minus'
+        } else {
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
 })
