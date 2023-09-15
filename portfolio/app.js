14b2fb93aecbefddab499201c3e417821934e21f
Szymon Zych <szymonzych868@gmail.com>
2023-09-15 20:45:30 +0200

    creating an about me section and media (850px)

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,7 +1,18 @@
 let MenuBtn = document.getElementById('MenuBtn')
 
-MenuBtn.addEventListener('click',function(e){
+MenuBtn.addEventListener('click', function (e) {
     document.querySelector('body').classList.toggle('mobile-nav-active')
 
     this.classList.toggle('fa-xmark')
-} )
+})
+
+//animation 
+
+let typed = new Typed('.auto-input', {
+    //tekst do zmiany 
+    strings: ['Front-End Developer!', 'Technik Geodeta!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
