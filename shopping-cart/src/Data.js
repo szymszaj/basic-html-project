12e0d1aacebe578619a8b9c45def8a8925c0abe6
Szymon Zych <szymonzych868@gmail.com>
2023-10-09 21:22:48 +0200

    shopItemsData exportation

---
+++ shopping-cart/src/Data.js
@@ -0,0 +1,29 @@
+let shopItemsData = [{
+    id: "TB789",
+    name: "Casual shirt",
+    price: 45,
+    desc: "This casual shirt is crafted from comfortable, lightweight fabric and boasts a relaxed fit. Its versatile design makes it suitable for various informal occasions.",
+    img: "images/img-1.jpg"
+},
+{
+    id: "SKU7890",
+    name: "Office shirt",
+    price: 100,
+    desc: "This office shirt is tailored with precision, offering a sharp, professional appearance. Its crisp white fabric and classic button-down collar make it a timeless choice for the workplace.",
+    img: "images/img-2.jpg"
+},
+{
+    id: " Kent456",
+    name: "T shirt",
+    price: 25,
+    desc: "This T-shirt is made of soft, breathable cotton and features a classic crew neckline. Its vibrant blue color adds a pop of style to any outfit.",
+    img: "images/img-3.jpg"
+},
+{
+    id: " T955876",
+    name: "Mens Suit",
+    price: 599,
+    desc: "Featuring a well-tailored jacket and matching trousers. The timeless design and fine craftsmanship make it a perfect choice for formal occasions and business settings.",
+    img: "images/img-4.jpg"
+},
+]
