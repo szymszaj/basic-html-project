6a1d6c23f01eff5faeea766477bf63a6332aea53
Szymon Zych <szymonzych868@gmail.com>
2023-10-10 15:35:38 +0200

    the amount of things you have

--- shopping-cart/src/cart.js
+++ shopping-cart/src/cart.js
@@ -57,3 +57,44 @@ let generateCartItem = () => {
     }
 }
 generateCartItem()
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+
+    // console.log(basket);
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+}
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    generateCartItem()
+    localStorage.setItem("data", JSON.stringify(basket))
+
+}
+
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation()
+}
