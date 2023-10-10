a18c15c58703d86facf41ee169b3db7612d36fee
Szymon Zych <szymonzych868@gmail.com>
2023-10-10 15:31:56 +0200

    card styling with shopping cart

--- shopping-cart/src/cart.js
+++ shopping-cart/src/cart.js
@@ -11,6 +11,40 @@ calculation();
 
 let generateCartItem = () => {
     if (basket.length !== 0) {
+        return ShoppingCart.innerHTML = basket
+            .map((x) => {
+                let { id, item } = x
+                let search = shopItemsData.find((z) => z.id === id) || []
+                return `
+            <div class="cart-item">
+            <img width="100" src=${search.img} alt="" />
+
+            <div class="details">
+                <div class="title-price-x">
+                <h4 class="title-price">
+                <p> ${search.name}</p>
+                <p class="cart-item-price">$ ${search.price}</p>
+
+                </h4>
+                <i class="fa-solid fa-xmark"></i>
+                </div>
+
+                <div class="cart-buttons">
+                    <div class="buttons">
+                    <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+                    <div id=${id} class="quantity">${item}</div>
+                    <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+                     </div>
+                </div>
+
+                <h3></h3>
+             </div>
+             </div>
+            
+            
+            
+            `
+            })
     }
     else {
         ShoppingCart.innerHTML = ``
