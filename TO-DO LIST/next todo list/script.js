dcd712107884da2a987984c0d0e81b10d33a6fb2
Szymon Zych <szymonzych868@gmail.com>
2024-07-04 22:39:13 +0200

    removing unused code

--- TO-DO LIST/next todo list/script.js
+++ TO-DO LIST/next todo list/script.js
@@ -1,50 +1,30 @@
-// let inputBx = document.querySelector('#inputBx');
-// let list = document.querySelector('#list');
+let inputBx = document.querySelector("#inputBx");
+let list = document.querySelector("#list");
 
-// inputBx.addEventListener("keyup", function(event) {
-//     if (encodeURIComponent.key == "Enter") {
-//         addItem(this.value)
-//         this.value = ""
-//     }
-// })
-
-// let addItem = (inputBx) => {
-//     let listItem = document.createElement("li")
-//     listItem.innerHTML = `${inputBx}<i><i>`;
-
-//         list.appendChild(listItem);
-// }
-
-let inputBx = document.querySelector('#inputBx');
-let list = document.querySelector('#list');
-
-inputBx.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        addItem(this.value);
-        this.value = "";
-    }
+inputBx.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    addItem(this.value);
+    this.value = "";
+  }
 });
 
 let addItem = (taskDescription) => {
-    if (taskDescription.trim() !== "") {
-        let listItem = document.createElement("li");
-        listItem.textContent = taskDescription;
+  if (taskDescription.trim() !== "") {
+    let listItem = document.createElement("li");
+    listItem.textContent = taskDescription;
 
+    listItem.addEventListener("click", function () {
+      this.classList.toggle("done");
+    });
 
-        listItem.addEventListener("click", function(){
-            this.classList.toggle('done')
-        })
-        
-        let removeButton = document.createElement("i");
-        removeButton.className = "remove-button";
-        removeButton.textContent = "";
-        removeButton.addEventListener("click", function() {
-            list.removeChild(listItem);
-        });
-        
-        listItem.appendChild(removeButton);
-        list.appendChild(listItem);
-    }
+    let removeButton = document.createElement("i");
+    removeButton.className = "remove-button";
+    removeButton.textContent = "";
+    removeButton.addEventListener("click", function () {
+      list.removeChild(listItem);
+    });
+
+    listItem.appendChild(removeButton);
+    list.appendChild(listItem);
+  }
 };
-
-
